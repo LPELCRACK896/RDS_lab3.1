a23298f70713cabb7f67efb52b32bcc8e4307ffc
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-09-21 10:52:59 -0600

    Add methods to colors

--- src/main/java/xmpp_network/Colors.java
+++ src/main/java/xmpp_network/Colors.java
@@ -11,10 +11,35 @@ public class Colors {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
-
-    public String blackText(String text){
+    public String blackText(String text) {
         return ANSI_BLACK + text + ANSI_RESET;
     }
 
+    public String redText(String text) {
+        return ANSI_RED + text + ANSI_RESET;
+    }
 
+    public String greenText(String text) {
+        return ANSI_GREEN + text + ANSI_RESET;
+    }
+
+    public String yellowText(String text) {
+        return ANSI_YELLOW + text + ANSI_RESET;
+    }
+
+    public String blueText(String text) {
+        return ANSI_BLUE + text + ANSI_RESET;
+    }
+
+    public String purpleText(String text) {
+        return ANSI_PURPLE + text + ANSI_RESET;
+    }
+
+    public String cyanText(String text) {
+        return ANSI_CYAN + text + ANSI_RESET;
+    }
+
+    public String whiteText(String text) {
+        return ANSI_WHITE + text + ANSI_RESET;
+    }
 }
